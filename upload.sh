c6f8ecf028864ba69f55767388b240b499e649bf
Kama Kepar <yptiza@example.com>
2020-11-04 18:13:15 +0000

    All Updated

---
+++ upload.sh
@@ -0,0 +1,8 @@
+git init
+git config --global user.name "Kama Kepar"
+git config --global user.email "yptiza@example.com"
+git add *
+git commit -m "All Updated"
+git branch -M main
+git remote add origin https://github.com/Kamakepar2029/nsh.git
+git push -u origin main
